7e628486a5b6c6ad931a5f802b743b2bd03e08b2
Abhay <abhayparasharr@gmail.com>
2022-04-26 08:50:23 +0530

    Checking after some time

---
+++ assn5/Assignment5/Debug/src/HelloWorld.d
@@ -0,0 +1,7 @@
+src/HelloWorld.o: ../src/HelloWorld.c \
+  /opt/arm/developmentstudio-2021.1/sw/ARMCompiler6.16/bin/../include/stdio.h \
+  /opt/arm/developmentstudio-2021.1/sw/ARMCompiler6.16/bin/../include/stdlib.h
+
+/opt/arm/developmentstudio-2021.1/sw/ARMCompiler6.16/bin/../include/stdio.h:
+
+/opt/arm/developmentstudio-2021.1/sw/ARMCompiler6.16/bin/../include/stdlib.h:
